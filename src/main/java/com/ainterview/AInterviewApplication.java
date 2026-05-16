1246ea04df583509eff115970a819c24a469e57e
waqarromaisha <romaishawaqar0786@gmail.com>
2026-05-16 18:26:04 +0530

    demo for testing

--- src/main/java/com/ainterview/AInterviewApplication.java
+++ src/main/java/com/ainterview/AInterviewApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AInterviewApplication {
+	// this is the ai project
 
 	public static void main(String[] args) {
 		SpringApplication.run(AInterviewApplication.class, args);
